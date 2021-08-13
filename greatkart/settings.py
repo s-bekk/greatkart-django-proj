ad9202e9fcb1675554cd999337e2b62f1fe829f5
Samuel Bekker <sb6187@nyu.edu>
2021-08-13 17:40:29 -0400

    Updated Allowed Host

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['django-env.eba-ezffk5mm.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['great-kart-proj-env.eba-sf8eey2i.us-east-2.elasticbeanstalk.com']
 
 
 # Application definition
